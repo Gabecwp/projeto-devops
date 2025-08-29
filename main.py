5dddbde86d108c572cb22a6913840eddd692e719
Gabecwp <ieielctba@hotmail.com>
2025-08-28 22:18:24 -0300

    alteracao-pre-docker

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import random
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
 
 @app.get("/helloworld")
 async def root():
@@ -11,4 +17,19 @@ async def root():
 # 127.0.0.1:8000/teste1
 @app.get("/funcaoteste")
 async def funcaoteste1():
-    return {"teste": True, "num_aleatorio": random.randint(1, 100)}
+    return {"teste": True, "num_aleatorio": random.randint(1, 1000)}
+
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_estudante(id_estudante: int):
+    return id_estudante > 0
+
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
